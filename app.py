4030ef668b038f6b8aae612a1bf27b8f77fd7f08
HabibaSanad <habibaabdelmalik477@gmail.com>
2025-12-03 15:11:06 +0200

    Updated

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
+import locale
+locale.setlocale(locale.LC_NUMERIC, 'en_US.UTF-8')
+
 
 # Load model
 model = joblib.load("car_price_model.pkl")
